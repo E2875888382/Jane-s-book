50aeb7c4a2fcd9c949808ea25e002af60adc8116
E2875888382 <2875888382@qq.com>
2019-07-20 09:36:41 +0800

    init

--- src/router.js
+++ src/router.js
@@ -8,7 +8,10 @@ import Index from './main/index.vue'
 import UserPage from './main/userPage.vue'
 import News from './main/news.vue'
 import NewsDetails from './main/news/newDetails.vue'
-import Comic from './main/comic/comic_index.vue'
+import Comic_menu from './main/comic/comic_menu.vue'
+import Comic_index from './main/comic/comic_index.vue'
+import Comic_ranking from './main/comic/comic_ranking.vue'
+import Comic_classification from './main/comic/comic_classification.vue'
 
 //手动安装vue-router
 Vue.use(VueRouter)
@@ -20,7 +23,14 @@ var router=new VueRouter({
         { path:'/userPage',component:UserPage },
         { path:'/news',component:News },
         { path:'/newsDetails/:id',component:NewsDetails },
-        { path:'/comic',component:Comic },
+        { path:'/comic',component:Comic_menu,
+            children:
+            [
+                { path:'ranking',component:Comic_ranking },
+                { path:'index',component:Comic_index },
+                { path:'classification',component:Comic_classification }
+            ]
+        },
     ]
 })
 
