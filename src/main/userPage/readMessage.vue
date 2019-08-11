83b836e43f04625997c11e90d53637139841e847
E2875888382 <2875888382@qq.com>
2019-08-11 18:08:28 +0800

    修改消息表格样式

--- src/main/userPage/readMessage.vue
+++ src/main/userPage/readMessage.vue
@@ -3,23 +3,23 @@
     <div class="messageBox">
         <el-tabs value="first">
             <el-tab-pane :label="'未读消息('+$store.state.messageCount+')'" name="first">
-                <el-table :data="$store.state.message" style="width: 100%">
-                    <el-table-column prop="content" min-width="500px"> </el-table-column>
-                    <el-table-column prop="time"> </el-table-column>
-                    <el-table-column prop="nickName"> </el-table-column>
+                <el-table :data="$store.state.message" style="width: 100%" :default-sort = "{prop: 'time', order: 'descending'}" :cell-style="cellStyle">
+                    <el-table-column label="消息内容" prop="content" min-width="500px" header-align="center"> </el-table-column>
+                    <el-table-column label="时间" prop="time" sortable header-align="center" align="center"> </el-table-column>
+                    <el-table-column prop="nickName" align="center"> </el-table-column>
                     <el-table-column align="right">
                         <template slot-scope="scope">
-                            <el-button size="mini" type="primary" @click="isRead(scope.row.messageID)">标为已读</el-button>
+                            <el-button size="mini" type="primary" plain @click="isRead(scope.row.messageID)">标为已读</el-button>
                         </template>
                     </el-table-column>
                     <div slot="empty" class="empty"> </div>
                 </el-table>
             </el-tab-pane>
             <el-tab-pane :label="'未读消息('+$store.state.historyMessage.length+')'" name="second">
-                <el-table :data="$store.state.historyMessage" style="width: 100%">
-                    <el-table-column prop="content" min-width="500px"> </el-table-column>
-                    <el-table-column prop="time"> </el-table-column>
-                    <el-table-column prop="nickName"> </el-table-column>
+                <el-table :data="$store.state.historyMessage" style="width: 100%" :default-sort = "{prop: 'time', order: 'descending'}" :cell-style="cellStyle">
+                    <el-table-column label="消息内容" prop="content" min-width="500px" header-align="center"> </el-table-column>
+                    <el-table-column label="时间" prop="time" sortable header-align="center" align="center"> </el-table-column>
+                    <el-table-column prop="nickName" align="center"> </el-table-column>
                     <div slot="empty" class="empty"> </div>
                 </el-table>
             </el-tab-pane>
@@ -62,6 +62,19 @@ export default{
                 this.$store.commit('getHistoryMessage',result.body);
             })
         },
+        cellStyle({row, column, rowIndex, columnIndex}){
+            if(columnIndex === 0){
+                return {'font-size':'14px','text-indent':'2em' }
+            }
+            else if(columnIndex === 2){ //指定坐标
+                return {'color':'#999','font-size':'12px'}
+            }else if(columnIndex === 1){
+                return {'color':'#999','font-size':'12px'}
+            }
+            else{
+                return ''
+            }
+        }
     }
 }
 </script>
