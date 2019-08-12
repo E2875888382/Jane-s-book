4b4a3aeecc1ebf9252e07872ab44f5a32f011b85
E2875888382 <2875888382@qq.com>
2019-08-12 14:45:16 +0800

    使用连接池管理数据库连接

--- server/mysql.js
+++ server/mysql.js
@@ -1,40 +1,28 @@
 const mysql = require('mysql');
 
-var connection=mysql.createConnection({
+var pool = mysql.createPool({
     host:'localhost',
     user:'root',
     password:'12345',
     port: '3306', 
     database:'videos',
     multipleStatements: true // 支持执行多条 sql 语句
-});
-
-//https://blog.csdn.net/bipedal_bit/article/details/48246963
-//用于处理第二次连接数据库出现error的问题
-function handleDisconnect(connection) {
-    connection.on('error', function(err) {
-      if (!err.fatal) {
-        return;
-      }
-      if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
-        throw err;
-      }
-      console.log('Re-connecting lost connection: ' + err.stack);
-      connection = mysql.createConnection(connection.config);
-      handleDisconnect(connection);
-      connection.connect();
-    });
-};
+})
 
 module.exports = function(sql,callback){
-        connection.connect();
-        connection.query(sql,(error,result) => {
-            if (error){
-                response.status(500).send('server error');
-            }
-            callback(result);
-        });
-        handleDisconnect(connection);
+    pool.getConnection((error,connection)=>{
+        if(error){
+            throw error;
+        }else{
+            connection.query(sql,(error,result) => {
+                if (error){
+                    response.status(500).send('server error');
+                }
+                connection.release();
+                callback(result);
+            });
+        }
+    })
 }
 
 
